0cd70afc8f3032af1f804b267fc03ff90c32d58c
PaulSigua <mateosigua2002@gmail.com>
2024-12-06 00:18:53 -0500

    Endpoints

--- app.py
+++ app.py
@@ -20,31 +20,31 @@ try:
     def api_agregar_cliente(cliente: models.Cliente):
         return main.agregar_cliente(cliente)
 
-    @app.post("/servicios")
+    @app.post("/servicios", description="Endpoint para crear los SERVICIOS")
     def api_agregar_servicio(servicio: models.Servicio):
         return main.agregar_servicio(servicio)
 
-    @app.post("/habitaciones")
+    @app.post("/habitaciones", description="Endpoint para crear las HABITACIONES")
     def api_agregar_habitacion(habitacion: models.Habitacion):
         return main.agregar_habitacion(habitacion)
 
-    @app.post("/reservas")
+    @app.post("/reservas", description="Endpoint para crear las RESERVAS")
     def api_agregar_reserva(reserva: models.Reserva):
         return main.agregar_reserva(reserva)
 
-    @app.post("/facturas")
+    @app.post("/facturas", description="Endpoint para crear las FACTURAS")
     def api_agregar_factura(factura: models.Factura):
         return main.agregar_factura(factura)
 
-    @app.post("/pagos")
+    @app.post("/pagos", description="Endpoint para crear los PAGOS")
     def api_agregar_pago(pago: models.Pago):
         return main.agregar_pago(pago)
 
-    @app.get("/")
+    @app.get("/", description="Endpoint RAIZ")
     def home():
-        return {"message": "API para Reservas de Hotel funcionando ..."}
+        return {"message": "API para Reservas de Hotel en ejecución ..."}
 except Exception as e:
-    print(f"Error en los enpoints, {e}")
+    print(f"ERROR, fallaron los enpoints, {e}")
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=9999, reload=True)
